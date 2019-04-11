6ca4b361493e7e48e3c3cf6b1ed95f8a1b2b724c
Tyler <tylernathanreed@gmail.com>
2019-04-11 11:37:54 -0500

    ~ Let's try that again

--- src/FlashServiceProvider.php
+++ src/FlashServiceProvider.php
@@ -11,41 +11,41 @@ use Illuminate\Support\ServiceProvider;
  */
 class FlashServiceProvider extends ServiceProvider
 {
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['flash'] = $this->app->singleton(function() {
-			return new Flash;
-		});
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
     {
-		return ['flash'];
-	}
+        $this->app->singleton('flash', function() {
+            return new Flash;
+        });
+    }
 
-	/**
-	 * Perform post-registration booting of services.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$path = __DIR__ . '/../resources/views';
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['flash'];
+    }
 
-		$this->loadViewsFrom($path, 'flash');
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $path = __DIR__ . '/../resources/views';
 
-		$this->publishes([
-			$path => base_path('resources/views/vendor/flash'),
-		], 'flash');
-	}
+        $this->loadViewsFrom($path, 'flash');
+
+        $this->publishes([
+            $path => base_path('resources/views/vendor/flash'),
+        ], 'flash');
+    }
 }
